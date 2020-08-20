0ef670cea6f2b8e56b4028a24e03f6f9054b20fa
b1acksmyth <smith.spence13@gmail.com>
2020-08-19 22:53:02 -0700

    Test file made and added

--- app.js
+++ app.js
@@ -14,40 +14,127 @@ const { listenerCount } = require("process");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-function getInfo() {
-  inquirer.prompt(
-    {
-      type: "input",
-      name: "name",
-      message: "What is this employee's name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is this employee's ID number?"
-    },
-    {
-      type: "list",
-      name: "role",
-      message: "What is the role for this employee?",
-      choices: ["Manager", "Engineer", "Intern"],
-    },)
-  .then(answers => {
-    if (answers.role === "Manager") {
-      
+let employees = [];
+
+// Questions
+
+const initialQuestion = [
+  {
+    type: "list",
+    name: "role",
+    message: "What is the role for this employee?",
+    choices: ["Manager", "Engineer", "Intern", "Done!"],
+  },
+];
+
+const employeeQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is this employees name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is this employee's ID number?",
+    validate: id => {
+      let invalid = isNaN(id);
+      if (invalid) {
+        return "Please enter a valid number";
+      } else {
+        return true;
+      }
     }
-  })
-}
-inquirer.prompt()
-  .then(answers => {
-    
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is this employee's email?",
+  },
+];
+
+const managerQuestion = [
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is this employee's office phone number?",
+  },
+];
+
+const engineerQuestion = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is this employee's GitHub username?",
+  },
+];
+
+const internQuestion = [
+  {
+    type: "input",
+    name: "school",
+    message: "What school is this employee attending?",
+  },
+];
+
+function init() {
+  inquirer.prompt(initialQuestion).then(answer => {
+    switch (answer.role) {
+      case "Manager":
+        makeManager();
+        break;
+      case "Engineer":
+        makeEngineer();
+        break;
+      case "Intern":
+        makeIntern();
+        break;
+      case "Done!":
+        let website = render(employees);
+        fs.writeFile(outputPath, website, (err) => {
+          if (err) throw err;
+          console.log("File created!");
+        });
+        break;
+    };
   }).catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
+    if (error) {
+      return
+    };
   });
+};
+
+function makeManager() {
+  inquirer.prompt(employeeQuestions.concat(managerQuestion)).then(answers => {
+    let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    employees.push(manager);
+    init();
+  });
+};
+
+function makeEngineer() {
+  inquirer.prompt(employeeQuestions.concat(engineerQuestion)).then(answers => {
+    let engineer = new Engineer(answers.name, answers.id, answers.email, answers.officeNumber);
+    employees.push(engineer);
+    init();
+  });
+};
+
+function makeIntern() {
+  inquirer.prompt(employeeQuestions.concat(internQuestion)).then(answers => {
+    let intern = new Intern(answers.name, answers.id, answers.email, answers.officeNumber);
+    employees.push(intern);
+    init();
+  });
+};
+
+//Start App
+init();
+
+// fs.writeFile("team.html", outputPath, (err) => {
+//   if (err) throw err;
+//   console.log("File created!");
+// });
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
